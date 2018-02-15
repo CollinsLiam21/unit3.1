368ee54390ea7d989b61342e59778a81d06e4fd3
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-15 08:16:40 -0500

    Updated from Brython Server: 2/15/2018 8:16:39 AM

--- 1stDemo.py
+++ 1stDemo.py
@@ -1,1 +1,51 @@
+#Liam Collins
+#2/15/18
+#1stDemo.py - how to use for and while loops
 
+#I	love	comp	sci	5	times
+for	i	in	range(0,5):
+				print('I	<3	Computer	Science')
+
+
+#while	loop	version
+i	=	0
+while	i<5:
+				print('I	<3	Computer	Science')
+				i	=	i+1	#update	step
+
+
+#Counts	from	1	to	10
+for	i	in	range(1,11):
+				print(i)
+
+
+i	=	1
+while	i<=	10:
+				print(i)
+				i	+=	1	#means	add	one	to	i
+
+
+#Count	from	27	to	43	by	2s
+for	i	in	range(27,44,2):
+				print(i)
+
+
+i	=	27
+while	i	<=	43:
+				print(i)
+				i	+=	2
+
+
+#add	up	the	numbers	from	1	to	5
+total	=	0
+for	i	in	range(1,6):
+				total	=	total	+	i
+print(total)
+
+
+total	=	0
+i	=	1
+while	i	<=	5:
+				total	=	total	+	i
+				i	+=	1
+print(total)
