dbce45ebfee72b6adbd55770ed9f73b8f18c68fc
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-15 08:30:39 -0500

    Updated from Brython Server: 2/15/2018 8:30:39 AM

--- fives.py
+++ fives.py
@@ -1,1 +1,16 @@
+#Liam Collins
+#2/15/18
+#fives.py
 
+num = int(input('Enter a number: '))
+
+#i = 1
+#while i < num:
+    #if i%5 == 0:
+        #print(i)
+    #i += 1
+    
+i = 5
+while i <= num:
+    print(i)
+    i += 5
