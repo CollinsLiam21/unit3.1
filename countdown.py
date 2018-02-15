08ec00879040b1d2ff57c484123b6b1292cb7144
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-15 08:39:05 -0500

    Updated from Brython Server: 2/15/2018 8:39:04 AM

--- countdown.py
+++ countdown.py
@@ -1,1 +1,12 @@
+#Liam Collins
+#2/15/18
+#countdown.py
 
+num = int(input('Enter the starting number of countdown: '))
+
+i = num
+while i>0:
+    print(i)
+    i -= 1
+    if i == 0:
+        print('BOOM!')
