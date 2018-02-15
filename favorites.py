90fd35b2e28ef45f82c39a51966447009144d36e
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-15 08:21:20 -0500

    Updated from Brython Server: 2/15/2018 8:21:20 AM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,11 @@
+#Liam Collins
+#2/15/18
+#favorites.py
 
+word = input('Enter your favorite word: ')
+num = int(input('Enter your favorite number: '))
+
+i = 0
+while i < num:
+    print(word)
+    i += 1
