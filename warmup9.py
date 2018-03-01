503061374bfdbf8359810e293e6a284001991ebf
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-01 08:28:21 -0500

    Updated from Brython Server: 3/1/2018 8:28:20 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,10 @@
+#Liam Collins
+#3/1/18
+#warmup9.py
 
+text = input('Enter text: ')
+for ch in text:
+    if ch == 'i' or ch == 'o' or ch == 'e' or ch == 'a' or ch == 'u':
+        print(ch.upper())
+    else:
+        print(ch)
