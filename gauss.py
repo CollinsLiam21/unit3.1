53e10ce9a2a0999fe97ba6fa42b96da6c37fe13b
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-02 08:48:31 -0500

    Updated from Brython Server: 3/2/2018 8:48:30 AM

--- gauss.py
+++ gauss.py
@@ -5,11 +5,15 @@
 start = int(input('Enter a starting number: '))
 stop = int(input('Enter an ending number: '))
 
+total = 0
+for i in range(start,stop+1):
+    total = total + i
+print(total)
+
 diff = int(input('Enter specific difference: '))
 
 total = 0
-for i in range (1,101):
+for i in range (1,101,diff):
     total = total + i
-    i += diff
 print(total)
 
